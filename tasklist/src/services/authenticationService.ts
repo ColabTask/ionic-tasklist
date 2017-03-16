f2e5904e996d873dbf6e1c6afc4e74a4fcb24143
RignonNoel <rignon.noel@openmailbox.org>
2017-03-16 11:10:08 -0400

    delete token not used

--- tasklist/src/services/authenticationService.ts
+++ tasklist/src/services/authenticationService.ts
@@ -6,7 +6,6 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class AuthenticationService {
   serverURL = 'http://198.27.119.182/api/v1';
-  token = '8d876539b674331b4e1cb1ec8bef31b2e9c29681';
 
   constructor (protected http:Http) {
   }
